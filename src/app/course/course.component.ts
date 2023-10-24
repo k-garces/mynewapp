54ab982b527543f953a59dc5da673cada8912dec
k-garces <kellygarce@gmail.com>
2023-10-24 17:45:08 -0500

    commit initial

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  courses: Array<Course> = [];
+  average: number = 11;
+
+  constructor(private courseService: CourseService) { }
+
+  getCourses() {
+    console.log("iniciando")
+    this.courseService.getCourses().subscribe(courses => {
+      this.courses = courses;
+    });
+  }
+
+  getAverageCredits () {
+    let sum = 0;
+    console.log("iniciando")
+        
+    for (let index = 0; index < this.courses.length; index++) { 
+       sum += this.courses[index].credits;
+       console.log(sum)
+    }
+    this.average = sum; // / this.courses.length;
+    console.log("terminando")
+  }
+
+  ngOnInit() {
+    this.getCourses();
+    this.getAverageCredits();
+  }
+
+}
